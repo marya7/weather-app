8f0aae94ba9f248ced0ede7a521e89ec5840c697
SOFTWERK\maria <maria.belinska@softwerk.ee>
2022-04-01 12:54:00 +0300

    add middleware and react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./style/App.scss";
 import Weather from "./views/Weather";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Weather />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Weather />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/api/store.js
+++ src/api/store.js
@@ -5,4 +5,6 @@ export const store = configureStore({
   reducer: {
     [weatherApi.reducerPath]: weatherApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(weatherApi.middleware),
 });
